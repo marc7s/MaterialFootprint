2503d030e8b1229dc423a89040181a921264e4e3
marc7s <marcus.schagerberg@gmail.com>
2022-11-11 16:26:17 +0100

    Changed to using SASS instead of CSS

--- Frontend/material-footprint/src/index.tsx
+++ Frontend/material-footprint/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.sass';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
