736c99d6d71db83ddfb4cb924ba8c50ee4b9b6df
BribalStorm <marcusj01@hotmail.se>
2022-11-14 15:30:02 +0100

    Moved Widget to configurator

--- Frontend/material-footprint/src/App.tsx
+++ Frontend/material-footprint/src/App.tsx
@@ -3,7 +3,6 @@ import './App.sass';
 
 /* Components */
 import Configurator from './Configurator/Configurator';
-import Widget from './Widget/Widget';
 
 /* Utilities */
 
@@ -18,7 +17,6 @@ function App() {
       </header>
       <div className="App-content">
         <Configurator></Configurator>
-        <Widget></Widget>
       </div>
       <footer>
         <a href="https://github.com/marc7s/MaterialFootprint" target="_blank" rel="noopener noreferrer">Source code</a>
---
+++ Frontend/material-footprint/src/Configurator/Widget/Widget.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import './Widget.sass';
+
+import { Model } from '../interfaces';
+import { getEmissions } from '../../API';
+import { Emission } from '../../shared/interfaces';
+import { WidgetProp } from '../props';
+import EmissionComponent from '../EmissionComponent/EmissionComponent';
+
+
+function Widget({ currentModel }: WidgetProp) {
+  const [emissions, setEmissions] = useState([] as Emission[]);
+
+  // Load emissions from API on first render
+  useEffect(() => {
+    async function loadEmissions() {
+      getEmissions(currentModel.parts).then(m => setEmissions(m));
+    }
+    loadEmissions();
+  }, []);
+
+  return (
+    <div className="flex-box">
+      <div className="text-container">
+        Total CO<sub>2</sub> emissions: 
+      </div>
+
+      <table>
+        <tr>
+          <th>Part</th><th>Material</th><th>CO<sub>2</sub></th><th>Water</th><th>Price</th>
+        </tr>
+        {
+          emissions.map(e => <EmissionComponent emission={e}></EmissionComponent>)
+        }
+      </table>
+    </div>
+  );
+}
+
+export default Widget;
