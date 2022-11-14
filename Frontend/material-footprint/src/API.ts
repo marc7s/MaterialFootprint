38fa33ec64b59abce428ace24dbab26e67f3a320
BribalStorm <marcusj01@hotmail.se>
2022-11-14 15:44:45 +0100

    Added function getEmissions with mock data

--- Frontend/material-footprint/src/API.ts
+++ Frontend/material-footprint/src/API.ts
@@ -1,15 +1,16 @@
 /* Components */
 
 /* Utilities */
-import { Model } from "./Configurator/interfaces";
-import seat from './assets/configurator/chair/Seat.png';
-import frame from './assets/configurator/chair/Frame.png';
-import armrests from './assets/configurator/chair/Armrests.png';
-import accent from './assets/configurator/chair/Accent.png';
+import { Model, ModelPart } from "Configurator/interfaces";
+
+import seat from 'assets/configurator/chair/Seat.png';
+import frame from 'assets/configurator/chair/Frame.png';
+import armrests from 'assets/configurator/chair/Armrests.png';
+import accent from 'assets/configurator/chair/Accent.png';
 
 /* Shared */
-import { Material } from "./shared/interfaces";
-import { uniqueID } from "./shared/utils";
+import { Emission, Material } from "shared/interfaces";
+import { uniqueID } from "shared/utils";
 
 
 // Get a list of all the materials from the API
@@ -103,6 +104,34 @@ export async function getModels(): Promise<Model[]> {
                 }
             ]
             
+        }
+    ]
+}
+
+// Get a list of all the materials from the API
+export async function getEmissions(modelParts: ModelPart[]): Promise<Emission[]> {
+    const materials = await getMaterials();
+    return [
+        {
+            partName: "Seat",
+            material: materials.find(material => material.name === 'Textile')!,
+            co2CountInKg: 100,
+            h2oCountInL: 200,
+            priceInDollar: 300
+        },
+        {
+            partName: "Armrests",
+            material: materials.find(material => material.name === 'Plastic')!,
+            co2CountInKg: 1000,
+            h2oCountInL: 2000,
+            priceInDollar: 3000
+        },
+        {
+            partName: "Frame",
+            material: materials.find(material => material.name === 'Steel')!,
+            co2CountInKg: 10000,
+            h2oCountInL: 20000,
+            priceInDollar: 30000
         }
     ]
 }
