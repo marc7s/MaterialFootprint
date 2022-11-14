9e9676955eb7e386ab48b602661803b47e7bd40a
Hannes Kaulio <kaulio@student.chalmers.se>
2022-11-14 15:31:24 +0100

    fix typos

--- Backend/api/server/validator.ts
+++ Backend/api/server/validator.ts
@@ -5,21 +5,21 @@ import { Request, Response, NextFunction } from 'express';
 export function validateEmissionsInput(req: any, res: Response, next: NextFunction): void {
     
     // TODO remove hardcoded checks, make general function
-    if(!req.body.clientId || !req.body.area || !req.body.volume || !req.body.material || !req.body.surfaceTreatment)
+    if(!req.body.clientID || !req.body.area || !req.body.volume || !req.body.materialID || !req.body.surfaceTreatmentID)
         return next(new ApiRequestMalformedError('Missing required parameters. Required parameters: clientId, area, volume, material, surfaceTreatment'));
-    if (req.body.clientId < 0)
-        return next(new ApiRequestMalformedError('clientId must be a positive number'));
+    if (req.body.clientID < 0)
+        return next(new ApiRequestMalformedError('clientID must be a positive number'));
     if (req.body.area < 0)
         return next(new ApiRequestMalformedError('area must be a positive number'));
     if (req.body.volume < 0)
         return next(new ApiRequestMalformedError('volume must be a positive number'));
-    if (req.body.material < 0)
-        return next(new ApiRequestMalformedError('material must be a positive number'));
+    if (req.body.materialID < 0)
+        return next(new ApiRequestMalformedError('materialID must be a positive number'));
     
-    req.clientId = req.body.clientId;
+    req.clientID = req.body.clientID;
     req.area = req.body.area;
     req.volume = req.body.volume;
-    req.material = req.body.material;
-    req.surfaceTreatment = req.body.surfaceTreatment;
+    req.materialID = req.body.materialID;
+    req.surfaceTreatmentID = req.body.surfaceTreatmentID;
     next();
 }
