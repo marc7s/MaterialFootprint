92ca2772cfc614d54681c1776a6058c901315c05
BribalStorm <marcusj01@hotmail.se>
2022-11-14 15:30:02 +0100

    Moved Widget to configurator

--- Frontend/material-footprint/src/App.tsx
+++ Frontend/material-footprint/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.sass';
 import Configurator from './Configurator/Configurator';
-import Widget from './Widget/Widget';
 
 
 function App() {
@@ -12,7 +11,6 @@ function App() {
       </header>
       <div className="App-content">
         <Configurator></Configurator>
-        <Widget></Widget>
       </div>
       <footer>
         <a href="https://github.com/marc7s/MaterialFootprint" target="_blank">Source code</a>
