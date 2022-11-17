00eacbfb37b263bdca488fc220d1cfd8a2eac6d1
BribalStorm <marcusj01@hotmail.se>
2022-11-17 23:34:21 +0100

    Changed the layout of the widgets information

--- Frontend/material-footprint/src/Configurator/EmissionComponent/EmissionComponent.tsx
+++ Frontend/material-footprint/src/Configurator/EmissionComponent/EmissionComponent.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
 import './EmissionComponent.sass';
-import { uniqueID } from '../../shared/utils';
 import { EmissionProp } from '../props';
 
 function EmissionComponent({ emission }: EmissionProp) {
-
   // Render an EmissionComponent for each part in the model
   return (
-      <tr>
-        <td className="string">{ emission.partName }</td>
-        <td className="string">{ emission.material.name }</td>
-        <td className="number">{ emission.co2CountInKg }</td>
-        <td className="number">{ emission.h2oCountInL }</td>
-        <td className="number">{ emission.priceInDollar }</td>
-      </tr>
+    <div className="component">
+      <h3>Component: { emission.partName }</h3>
+      <table>
+        <tr><td className="string">CO<sub>2</sub>:</td><td className="number">{ emission.co2CountInKg }  </td></tr>
+        <tr><td className="string">Water:</td><td className="number">{ emission.h2oCountInL }   </td></tr>
+        <tr><td className="string">Cost:</td><td className="number">{ emission.priceInDollar } </td></tr>
+      </table>
+    </div>
+      // <tr>
+      //   <td className="string">{ emission.partName }</td>
+      //   <td className="string">{ emission.material.name }</td>
+      //   <td className="number">{ emission.co2CountInKg }</td>
+      //   <td className="number">{ emission.h2oCountInL }</td>
+      //   <td className="number">{ emission.priceInDollar }</td>
+      // </tr>
   );
 }
 
