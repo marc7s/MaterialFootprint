6ee882a81a47e3badca5c9880ced1030163f2908
Hannes Kaulio <kaulio@student.chalmers.se>
2022-11-17 22:21:46 +0100

    Add validator tests

--- Backend/api/tests/validator.test.ts
+++ Backend/api/tests/validator.test.ts
@@ -5,6 +5,56 @@ import { validateEmissionsInput } from "../server/validator";
 const emptyReq: Request = {
     body: {}
 } as Request;
+
+const validReq: Request = {
+    body: {
+        clientID: 1,
+        area: 1,
+        volume: 1,
+        materialID: 1,
+        surfaceTreatmentIDs: [1]
+    }
+} as Request;
+
+const negativeClientIDValReq: Request = {
+    body: {
+        clientID: -1,
+        area: 1,
+        volume: 1,
+        materialID: 1,
+        surfaceTreatmentIDs: [1]
+    }
+} as Request;
+
+const areaValIsStringReq: Request = {
+    body: {
+        clientID: 1,
+        area: "g",
+        volume: 1,
+        materialID: 1,
+        surfaceTreatmentIDs: [1]
+    }
+} as Request;
+
+const missingAreaReq: Request = {
+    body: {
+        clientID: 1,
+        volume: 1,
+        materialID: 1,
+        surfaceTreatmentIDs: [1]
+    }
+} as Request;
+
+const surfaceTreatmentIDsNotAllNumbersReq: Request = {
+    body: {
+        clientID: 1,
+        area: 1,
+        volume: 1,
+        materialID: 1,
+        surfaceTreatmentIDs: [1, "g"]
+    }
+} as Request;
+
 const emptyRes: Response = {} as Response;
 const next: NextFunction = (a: any) => a;
 
@@ -12,4 +62,33 @@ test('validateEmissionsInput with no parameters', () => {
     const response = validateEmissionsInput(emptyReq, emptyRes, next);
     expect(response).toBeInstanceOf(ApiRequestMalformedError);
     expect((response as any as ApiRequestMalformedError).message).toContain('Missing required parameters');
+});
+
+test('validateEmissionsInput with negative parameter', () => {
+    const response = validateEmissionsInput(negativeClientIDValReq, emptyRes, next);
+    expect(response).toBeInstanceOf(ApiRequestMalformedError);
+    expect((response as any as ApiRequestMalformedError).message).toContain('clientID must be a positive number');
+});
+
+test('validateEmissionsInput with wrong datatype parameter', () => {
+    const response = validateEmissionsInput(areaValIsStringReq, emptyRes, next);
+    expect(response).toBeInstanceOf(ApiRequestMalformedError);
+    expect((response as any as ApiRequestMalformedError).message).toContain('area must be a number');
+});
+
+test('validateEmissionsInput with valid parameters', () => {
+    const response = validateEmissionsInput(validReq, emptyRes, next);
+    expect(response).toBeUndefined();
+});
+
+test('validateEmissionsInput missing area parameter', () => {
+    const response = validateEmissionsInput(missingAreaReq, emptyRes, next);
+    expect(response).toBeInstanceOf(ApiRequestMalformedError);
+    expect((response as any as ApiRequestMalformedError).message).toContain('Missing required parameters. Required parameters: clientID, area, volume, materialID, surfaceTreatmentIDs');
+});
+
+test('validateEmissionsInput surfaceTreatmentIDs with not all numbers', () => {
+    const response = validateEmissionsInput(surfaceTreatmentIDsNotAllNumbersReq, emptyRes, next);
+    expect(response).toBeInstanceOf(ApiRequestMalformedError);
+    expect((response as any as ApiRequestMalformedError).message).toContain('surfaceTreatmentIDs must be an array of numbers');
 });
