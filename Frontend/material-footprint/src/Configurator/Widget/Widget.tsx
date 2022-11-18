cc48ddda5f42185a557b91adf93f00a42bb5939a
BribalStorm <marcusj01@hotmail.se>
2022-11-18 12:05:52 +0100

    Added total emissions

--- Frontend/material-footprint/src/Configurator/Widget/Widget.tsx
+++ Frontend/material-footprint/src/Configurator/Widget/Widget.tsx
@@ -20,13 +20,22 @@ function Widget({ currentModel }: WidgetProp) {
       getEmissions(currentModel.parts).then(m => setEmissions(m));
     }
     loadEmissions();
-  }, []);
+  }, [currentModel]);
+
+  var totalCo2 = 0;
+  var totalWater = 0;
+  var totalCost = 0;
+  for (let i = 0; i < emissions.length; i++) {
+    totalCo2 += emissions[i].co2CountInKg;
+    totalWater += emissions[i].h2oCountInL;
+    totalCost += emissions[i].priceInDollar;
+  }
 
   return (
     <div className="flex-box">
-      <div className="text-container">
-        Total CO<sub>2</sub> emissions: 
-      </div>
+      <div className="text-container"> Total CO<sub>2</sub>: {totalCo2} kg</div>
+      <div className="text-container"> Total water: {totalWater} L</div>
+      <div className="text-container"> Total cost: {totalCost} sek</div>
       {
         emissions.map(e => <EmissionComponent key={uniqueID()} emission={e}></EmissionComponent>)
       }
