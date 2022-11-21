85075ded78ee10b94a066d80643ae8bbe2f81250
Hannes Kaulio <kaulio@student.chalmers.se>
2022-11-21 11:59:33 +0100

    fix materials endpoint

--- Backend/api/server/api.ts
+++ Backend/api/server/api.ts
@@ -5,6 +5,9 @@ dotenv.config({path: __dirname + '../.env'});
 import express, { Response, NextFunction, Router } from 'express';
 import { Material, Emission } from '@shared/interfaces';
 import { validateEmissionsInput } from './validator';
+import { fetchMaterials, fetchMaterialCostForCompany, fetchSurfaceTreatmentCostForCompany } from '../../database/dbInterface';
+import fs from 'fs';
+
 
 const router: Router = express.Router();
 
@@ -18,18 +21,16 @@ router.post('/emissions', validateEmissionsInput, (req: any, res: Response, next
 
 });
 
-router.get('/materials', (req: any, res: Response) => {
+router.get('/materials', async (req: any, res: Response, next: NextFunction) => {
   log('Getting materials...');
-  // temporarily returns empty json
-  res.json([]);
-
-  //res.json(getMaterials(req, next));
-
+  res.json(await getMaterials(next)
+    .catch((err) => {next(err)}));
 });
 
-router.get('/models', (req: any, res: Response) => {
+router.get('/models', (req: any, res: Response, next: NextFunction) => {
   log('Getting models...');
   // temporarily returns empty json
+  getModels(req, next);
   res.json([]);
 
   //res.json(getModels(req, next));
@@ -40,14 +41,22 @@ async function calculateEmissions(req: any, next: NextFunction): Promise<Emissio
   return Promise.resolve([]);
 }
 
-async function getMaterials(req: any, next: NextFunction): Promise<Material[]> {
-  return Promise.resolve([]);
+async function getMaterials(next: NextFunction): Promise<Material[]> {
+  return fetchMaterials();
 }
 
 // Todo update signature to Model[]
 async function getModels(req: any, next: NextFunction): Promise<any[]> {
+  getModelFromCache(1);
   return Promise.resolve([]);
 }
 
+function getModelFromCache(modelID: number): any {
+  console.log('Getting model from cache...');
+  fs.mkdtemp('cache', (err, folder) => {
+    if (err) throw err;
+    console.log(folder);
+  });
+}
 
 module.exports = router;
