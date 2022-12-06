f4b45b8048874f3d9f9bcc5693de8c24bca3e0a8
marc7s <marcus.schagerberg@gmail.com>
2022-12-06 18:41:13 +0100

    Added changes

--- Frontend/material-footprint/src/Configurator/ModelComponent/ModelComponent.tsx
+++ Frontend/material-footprint/src/Configurator/ModelComponent/ModelComponent.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, { Suspense, useEffect, useState, useRef } from 'react';
 import './ModelComponent.sass';
+import * as THREE from 'three';
+import { Canvas, useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { GLTFExporter } from 'three/examples/jsm/exporters/GLTFExporter.js';
+import { OrbitControls, PerspectiveCamera, Preload, PresentationControls, Stars, Trail, View } from '@react-three/drei';
+
 
 /* Components */
 import ModelPart from 'Configurator/ModelPartComponent/ModelPartComponent';
@@ -7,8 +13,8 @@ import ModelPart from 'Configurator/ModelPartComponent/ModelPartComponent';
 /* Utilities */
 
 /* Shared */
-import { uniqueID } from 'shared/utils';
 import { Model, Size } from 'shared/interfaces';
+import Configurator from 'Configurator/Configurator';
 
 
 export interface ModelProp {
@@ -26,15 +32,101 @@ function ModelComponent({model, size, active, onModelChange}: ModelProp) {
     if(onModelChange) 
       onModelChange(model.id);
   };
-
+  /*
+fetch('http://localhost:5000/models').then(function(response) {
+  return response.json();
+}).then(function(response) {
+  console.log(response);
+});
+*/
   // Render a ModelPart for each part in the model
   return (
       <div className={'ModelComponent-model' + activeClass + sizeClass} onClick={onModelClick}>
         {
-          model.parts.map(part => <ModelPart key={uniqueID()} size={size} part={part}></ModelPart>)
+          <ConfiguratorTest model={model} />
         }
       </div>
   );
 }
+
+export interface ConfiguratorTestProp {
+  model: Model;
+}
+
+function ConfiguratorTest({ model }: ConfiguratorTestProp): JSX.Element {
+  const loader1 = useLoader(GLTFLoader, 'testmodel.glb')
+
+  // recursivly find all meshes in the model
+  findType(loader1.scene, 'Mesh', model)
+
+  const container: any = React.createRef()
+  const view1: any = React.createRef()
+
+  return ( 
+    <div ref = {container} style={{width: "100%", height: "100%"}}>
+   
+      <div className='bajs' ref={view1} style={{ position: 'relative', overflow: 'hidden', width: "100%", height: "100%"}} ></div>
+     
+
+     <Canvas eventSource={container} className="canvas">
+     
+      <View track={view1}>
+        <PerspectiveCamera
+          makeDefault
+          position={[100, 100, 100]}
+          fov={60}
+          zoom={0.9}
+        />
+        <OrbitControls target={[0,50,0]}>
+
+        </OrbitControls>
+    
+          <primitive object={loader1.scene.clone(true) } />
+      
+        <ambientLight intensity={0.5} />
+      </View>
+      <Preload all />
+
+</Canvas>
+    </div>     
+
+
+  )
+}
+
+function findType(object: any, type: any, model: Model) {
+  console.log(model)
+  object.children.forEach((child: any, index: any) => {
+      if (child.type === type) {
+        const partID = 1;
+        console.log(model.parts.find(m => m.id === partID)!.material.color)
+        child.material = new THREE.MeshStandardMaterial({ color: model.parts.find(m => m.id === partID)!.material.color})
+        
+        /*child.name = 'part' + index
+        if (child.name === 'part0') {
+          child.material = new THREE.MeshStandardMaterial({ color: 0xff0000})
+        }
+        if (child.name === 'part1') {
+          child.material = new THREE.MeshStandardMaterial({ color: 0x0000ff})
+        }
+        if (child.name === 'part2') {
+          child.material = new THREE.MeshStandardMaterial({ color: 0x00ff00})
+        }
+        if (child.name === 'part3') {
+          child.material = new THREE.MeshStandardMaterial({ color: 0x000000})
+        }*/
+        /*
+        if (index % 2 == 0) {
+          child.material = new THREE.MeshStandardMaterial({ color: Math.floor(Math.random()*16777215) })
+        } else {
+          child.material = new THREE.MeshStandardMaterial({ color: Math.floor(Math.random()*16777215) })
+        }
+        */
+          console.log(child.name);
+      }
+      findType(child, type, model);
+  });
+}
+
 
 export default ModelComponent;
